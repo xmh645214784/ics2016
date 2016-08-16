d2a1198de9ee0b1ace1935ce6a46770db7a6bc3d
tracer-ics2016 <tracer@njuics.org>
2016-08-16 19:46:09 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  19:46:09 up  3:35,  1 user,  load average: 1.01, 0.41, 0.20 d4e54adcaefac9016c7c691639dc9dbfe248dfc

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -12,7 +12,7 @@ extern CPU_state cpu;
 
 enum {
 	NOTYPE = 256, EQ,/* TODO: Add more token types */
-	NUM
+	NUM,HEXNUM,NEG,DEREF,NEQ,AND,OR,NOT,REG
 };
 
 static struct rule {
@@ -35,8 +35,14 @@ static struct rule {
 	{"\\(",'('},
 	{"\\)",')'},
 
+	{"0x[0-9]+",HEXNUM},//调换顺序 优先匹配0x
 	{"[0-9]+",NUM},
 
+	{"!=",NEQ},
+	{"&&",AND},
+	{"\\|\\|",OR},
+	{"!",NOT},
+	{"\\$[a-z]+",REG}
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -122,6 +128,15 @@ uint32_t expr(char *e, bool *success)
 	*success=true;
 	/* TODO: Insert codes to evaluate the expression. */
 	//panic("please implement me");
+	int i;
+	for(i = 0; i < nr_token; i ++) {
+		if(tokens[i].type == '*' && (i == 0 || (tokens[i - 1].type !=NUM&&tokens[i-1].type!=HEXNUM) )&&tokens[i-1].type!='('&&tokens[i-1].type!=')') {
+			tokens[i].type = DEREF;
+		}
+		if(tokens[i].type == '-' && (i == 0 || (tokens[i - 1].type !=NUM&&tokens[i-1].type!=HEXNUM) )&&tokens[i-1].type!='('&&tokens[i-1].type!=')') {
+			tokens[i].type = NEG;	
+		}
+	}
 	return eval(0,nr_token-1);
 }
 
@@ -177,10 +192,18 @@ bool zaiyiduipipeidekuohaozhong(int p,int q,int position)
 
 int getpriority(int fuhao)
 {
+	if(fuhao==DEREF||fuhao==NEG||fuhao==NOT)
+		return 6;
 	if(fuhao=='*'||fuhao=='/')
 		return 5;
 	if(fuhao=='+'||fuhao=='-')
 		return 4;
+	if(fuhao==EQ||fuhao==NEQ)
+		return 3;
+	if(fuhao==AND)
+		return 2;
+	if(fuhao==OR)
+		return 1;
 	return 0;
 }
 
@@ -199,6 +222,7 @@ int findthedominantoperatorposition(int p,int q)
 	}
 	int flag=-1;
 
+	int danmu=-1;//单目运算符用 标记第一个可能的dominant运算符
 
 	for(i=0;i<=q-p;i++)
 	{
@@ -206,6 +230,7 @@ int findthedominantoperatorposition(int p,int q)
 		{	if(flag==-1)
 			{
 				flag=i;
+				danmu=i;
 			}
 			else if(getpriority(tokens[p+i].type)<=getpriority(tokens[p+flag].type))
 			{
@@ -213,9 +238,21 @@ int findthedominantoperatorposition(int p,int q)
 			}
 		}
 	}
-	Log("dominate在%d",flag+p);
+
+
+	
+	//如果是单目运算符 为统治运算符 顺序应该为第一个
+	if(getpriority(tokens[flag+p].type)==6)
+	{
+		Log("dominate在%d",danmu+p);
+		return danmu+p;
+	}
+	else
+	{
+		Log("dominate在%d",flag+p);
 		return flag+p;
 	}
+}
 
 
 uint32_t eval(int p,int q)
@@ -231,6 +268,64 @@ uint32_t eval(int p,int q)
 		 */ 
 		if(tokens[p].type==NUM)
 			return atoi(tokens[p].str);
+		if(tokens[p].type==HEXNUM)
+			return strtol(tokens[p].str,NULL, 16);
+		if(tokens[p].type==REG)
+		{
+			if(strcmp(tokens[p].str,"$eax")==0)
+				return cpu.eax;
+			if(strcmp(tokens[p].str,"$edx")==0)
+				return cpu.edx;
+			if(strcmp(tokens[p].str,"$ecx")==0)
+				return cpu.ecx;
+			if(strcmp(tokens[p].str,"$ebx")==0)
+				return cpu.ebx;
+			if(strcmp(tokens[p].str,"$ebp")==0)
+				return cpu.ebp;
+			if(strcmp(tokens[p].str,"$esi")==0)
+				return cpu.esi;
+			if(strcmp(tokens[p].str,"$edi")==0)
+				return cpu.edi;
+			if(strcmp(tokens[p].str,"$esp")==0)
+				return cpu.esp;
+
+			if(strcmp(tokens[p].str,"$ax")==0)
+				return cpu.ax;
+			if(strcmp(tokens[p].str,"$dx")==0)
+				return cpu.dx;
+			if(strcmp(tokens[p].str,"$cx")==0)
+				return cpu.cx;
+			if(strcmp(tokens[p].str,"$bx")==0)
+				return cpu.bx;
+			if(strcmp(tokens[p].str,"$bp")==0)
+				return cpu.bp;
+			if(strcmp(tokens[p].str,"$si")==0)
+				return cpu.si;
+			if(strcmp(tokens[p].str,"$di")==0)
+				return cpu.di;
+			if(strcmp(tokens[p].str,"$sp")==0)
+				return cpu.sp;
+
+			if(strcmp(tokens[p].str,"$al")==0)
+				return cpu.al;
+			if(strcmp(tokens[p].str,"$dl")==0)
+				return cpu.dl;
+			if(strcmp(tokens[p].str,"$cl")==0)
+				return cpu.cl;
+			if(strcmp(tokens[p].str,"$bl")==0)
+				return cpu.bl;
+			if(strcmp(tokens[p].str,"$ah")==0)
+				return cpu.ah;
+			if(strcmp(tokens[p].str,"$dh")==0)
+				return cpu.dh;
+			if(strcmp(tokens[p].str,"$ch")==0)
+				return cpu.ch;
+			if(strcmp(tokens[p].str,"$bh")==0)
+				return cpu.bh;
+			assert(0);
+		}
+		if(tokens[p].type==NEG||tokens[p].type==NOT||tokens[p].type==DEREF)//单目运算符
+			return 0;
 		panic("解析数时发生错误\n");
 
 	}
@@ -252,6 +347,13 @@ uint32_t eval(int p,int q)
 			case '-': return val1 - val2;
 			case '*': return val1 * val2;
 			case '/': return val1 / val2;
+			case EQ : return val1 ==val2;
+			case NEQ: return val1 !=val2;
+			case AND: return val1 &&val2;
+			case OR : return val1 ||val2;
+			case NOT: return !val2;
+			case NEG: return -val2;
+			case DEREF:return swaddr_read(val2,4);
 			default: assert(0);
 		}
 	}
