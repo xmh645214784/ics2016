c5664ebad5100eb7b313b06748c007371fe1c438
tracer-ics2016 <tracer@njuics.org>
2016-09-06 21:03:22 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  21:03:22 up  3:10,  2 users,  load average: 0.47, 0.27, 0.20 61116012f7093b58349814598a1d3f693b66fec

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -13,8 +13,8 @@ static void do_execute() {
 	print_asm_template1();
 #elif SUFFIX==w
 	cpu.esp-=2;
-	swaddr_write(cpu.esp,2,cpu.eip);
-	cpu.eip=(eip+op_dest->imm)&0000FFFFH;
+	swaddr_write(cpu.esp,2,cpu.ip);
+	cpu.eip=(eip+op_dest->imm)&0x0000FFFF;
 	print_asm_template1();
 
 #endif
