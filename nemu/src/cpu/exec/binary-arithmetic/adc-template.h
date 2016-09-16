d274ab62eae10162563080c1ba1fdf790b93f19e
tracer-ics2016 <tracer@njuics.org>
2016-09-16 20:50:58 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  20:50:58 up  1:32,  2 users,  load average: 0.50, 0.42, 0.30 8d3d5fbedf2c54d569ce00deb981afaa5c502b

--- nemu/src/cpu/exec/binary-arithmetic/adc-template.h
+++ nemu/src/cpu/exec/binary-arithmetic/adc-template.h
@@ -3,11 +3,53 @@
 #define instr adc
 
 extern CPU_state cpu;
-
+bool is_even_number_of_1(uint32_t val);
 //通用执行函数
 static void do_execute() {
 
 	OPERAND_W(op_dest,op_src->val+op_dest->val+cpu.CF);
+
+		
+	DATA_TYPE_S src=(DATA_TYPE_S)op_src->val;
+	DATA_TYPE_S des=(DATA_TYPE_S)op_dest->val;
+	DATA_TYPE_S result=src+des+cpu.CF;
+//Log("目的操作数类型%d reg标号为%d 值为%d,",op_dest->type,op_dest->reg,op_dest->val);
+//Log("cmp %d-%d",src,des);
+
+//OF
+	if(src>0&&des<0&&result<0)
+		cpu.OF=1;
+	else if(src<0&&des>0&&result>0)
+		cpu.OF=1;
+	else
+		cpu.OF=0;
+
+//SF
+	if(result>=0)
+		cpu.SF=0;
+	else
+		cpu.SF=1;
+//ZF
+	if(result==0)
+		cpu.ZF=1;
+	else
+		cpu.ZF=0;
+
+
+
+
+//PF
+	if(is_even_number_of_1(result))
+		cpu.PF=1;
+	else
+		cpu.PF=0;
+
+//CF
+	//可能有BUG
+	if(result<0)
+		cpu.CF=1;
+	else
+		cpu.CF=0;	
 	print_asm_template2();
 }
 
