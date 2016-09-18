77b8e78ea7143572c70c963f0a094694f14a4e42
tracer-ics2016 <tracer@njuics.org>
2016-09-18 21:44:52 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  21:44:52 up  2:26,  2 users,  load average: 0.16, 0.30, 0.20 fc4a47aafbd674fd7861859d2f3c000bfac19597

--- nemu/src/cpu/exec/jcc/jcc-template.h
+++ nemu/src/cpu/exec/jcc/jcc-template.h
@@ -1,34 +1,353 @@
 #include "cpu/exec/template-start.h"
-#include "debug.h"
+#define make_helper_jcc(instr) make_helper(concat3(instr,_rel_,SUFFIX))
 
-//暂时
-#ifndef condition
-	#define condition e
 
+
+make_helper_jcc(ja)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.CF==0&&cpu.ZF==0)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
 #endif
-
-#define instr concat(j,condition)  
-	//j+condition
-
-
-extern CPU_state cpu;
-
-//通用执行函数
-static void do_execute() {
-
-//JE
-	if(cpu.ZF==1)
-	{	
-		cpu.eip+=(DATA_TYPE_S)op_src->val;
-
-		#if DATA_BYTE ==2
-			cpu.eip=cpu.eip&0x0000FFFF;
-		#endif
-	}
-	print_asm_template1();
+	return 2;
 }
 
-make_instr_helper(rel)
+make_helper_jcc(jae)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.CF==0)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
 
+make_helper_jcc(jb)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.CF==1)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+
+
+make_helper_jcc(jbe)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.CF==1||cpu.ZF==1)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+
+make_helper_jcc(jc)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.CF==1)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+
+
+#if DATA_BYTE==1
+make_helper_jcc(jcxz)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.cx==0)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+make_helper_jcc(jecxz)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.ecx==0)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+#endif
+
+make_helper_jcc(je)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.ZF==1)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+make_helper_jcc(jg)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.ZF==0&&cpu.SF==cpu.OF)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+make_helper_jcc(jge)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.SF==cpu.OF)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+
+make_helper_jcc(jl)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.SF!=cpu.OF)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+make_helper_jcc(jle)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.ZF==1||cpu.SF!=cpu.OF)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+make_helper_jcc(jna)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.CF==1||cpu.ZF==1)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+make_helper_jcc(jnae)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.CF==1)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+make_helper_jcc(jnb)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.CF==0)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+make_helper_jcc(jnbe)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.CF==0&&cpu.ZF==0)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+make_helper_jcc(jnc)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.CF==0)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+make_helper_jcc(jne)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.ZF==0)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+
+make_helper_jcc(jng)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.ZF==1||cpu.SF!=cpu.OF)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+
+make_helper_jcc(jnge)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.SF!=cpu.OF)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+make_helper_jcc(jnl)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.SF==cpu.OF)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+
+
+make_helper_jcc(jnle)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.ZF==0&&cpu.SF==cpu.OF)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+
+make_helper_jcc(jno)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.OF==0)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+
+make_helper_jcc(jnp)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.PF==0)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+
+make_helper_jcc(jns)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.SF==0)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+
+make_helper_jcc(jnz)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.ZF==0)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+
+
+make_helper_jcc(jo)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.OF==1)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+make_helper_jcc(jp)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.PF==1)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+
+make_helper_jcc(jpe)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.PF==1)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+make_helper_jcc(jpo)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.PF==0)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+make_helper_jcc(js)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.SF==1)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
+make_helper_jcc(jz)
+{
+	concat(decode_rel_,SUFFIX)(eip);
+	if(cpu.ZF==1)
+	cpu.eip+=op_src->val;
+#if DATA_BYTE==2
+	cpu.eip=cpu.eip&0xFFFF;
+#endif
+	return 2;
+}
 
 #include "cpu/exec/template-end.h"
+
+#ifdef make_helper_jcc
+#undef make_helper_jcc
+#endif
--- nemu/src/cpu/exec/ret/ret.c
+++ nemu/src/cpu/exec/ret/ret.c
@@ -7,7 +7,7 @@ make_helper(ret_w)
 	cpu.ip=swaddr_read(cpu.esp,2);
 	cpu.esp+=2;
 	cpu.eip=cpu.eip&0x0000FFFF;
-	print_asm("retw");
+	print_asm("ret");
 	return 0;
 }
 
@@ -15,7 +15,7 @@ make_helper(ret_l)
 {
 	cpu.eip=swaddr_read(cpu.esp,4);
 	cpu.esp+=4;
-	print_asm("retl");
+	print_asm("ret");
 	return 0;
 }
 
@@ -23,7 +23,9 @@ make_helper(ret_i_w)
 {
 	decode_i_w(eip);
 	int16_t a=op_src->val;
+	cpu.esp+=2;
 	cpu.esp+=a;
+	print_asm("ret imm16");
 	return 0;
 }
 
