806d0080fcc0fab93c82140d273b1e3868b4c3b3
tracer-ics2016 <tracer@njuics.org>
2016-09-19 09:47:09 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  09:47:09 up  1:32,  2 users,  load average: 0.13, 0.19, 0.20 9b5fc00e2d9de5e3c9f9532ca6cb3dd812fd6dea

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -368,7 +368,7 @@ uint32_t eval(int p,int q,bool *success)
 		//Log("%x %d",val2,val2);
 		switch(tokens[op].type) {
 			case '+': return val1 + val2;
-			case '-': panic("LJ");return val1 - val2;
+			case '-': return val1 - val2;
 			case '*': return val1 * val2;
 			case '/': return val1 / val2;
 			case EQ : return val1 ==val2;
