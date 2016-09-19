eb8febb52c53e7cfc140edda98dcda74ea6d46bb
tracer-ics2016 <tracer@njuics.org>
2016-09-19 08:30:05 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  08:30:05 up 15 min,  2 users,  load average: 0.75, 0.38, 0.35 6f8cf1d72b581d3f32baa39a3e6d0cf1e8e554d7

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -127,7 +127,7 @@ uint32_t expr(char *e, bool *success)
 		*success = false;
 		return 0;
 	}
-	//Log("match successfully");
+	Log("match successfully");
 	//*success=true;
 	/* TODO: Insert codes to evaluate the expression. */
 	//panic("please implement me");
