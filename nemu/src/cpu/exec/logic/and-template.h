d60b03f478f9da01a26dbe524ffad6070441a44a
tracer-ics2016 <tracer@njuics.org>
2016-09-29 09:40:12 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:41:41 UTC 2016 i686 i686 i686 GNU/Linux  09:40:12 up 33 min,  2 users,  load average: 0.30, 0.26, 0.44 518d209ee61a38f2f57bfeb47f8a85bf030de2d

--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -4,6 +4,7 @@
 
 static void do_execute () {
 	DATA_TYPE_S result = op_dest->val & op_src->val;
+	Log("des%x src:%x",op_dest->val,op_src->val);
 	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
