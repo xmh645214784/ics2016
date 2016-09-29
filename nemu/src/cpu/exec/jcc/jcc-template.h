c5d2f4d838c1d357fb4787b0ef7ead2657a0fb32
tracer-ics2016 <tracer@njuics.org>
2016-09-29 15:55:37 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:41:41 UTC 2016 i686 i686 i686 GNU/Linux  15:55:37 up 21 min,  2 users,  load average: 0.45, 0.23, 0.25 b5efdda4837e629c953020c3777f1839b89c1793

--- nemu/src/cpu/exec/jcc/jcc-template.h
+++ nemu/src/cpu/exec/jcc/jcc-template.h
@@ -7,10 +7,12 @@ make_helper_jcc(ja)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.CF==0&&cpu.ZF==0)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("ja_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -19,10 +21,12 @@ make_helper_jcc(jae)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.CF==0)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jae_rel_"str(SUFFIX)" %s",op_src->str);
 	
 	return 2;
@@ -32,10 +36,12 @@ make_helper_jcc(jb)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.CF==1)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jb_rel_"str(SUFFIX)" %s",op_src->str);
 	
 	return 2;
@@ -46,10 +52,12 @@ make_helper_jcc(jbe)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.CF==1||cpu.ZF==1)
+	{
 		cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jbe_rel_"str(SUFFIX)" %s",op_src->str);	
 	return 2;
 }
@@ -58,10 +66,12 @@ make_helper_jcc(jc)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.CF==1)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jc_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -72,10 +82,12 @@ make_helper_jcc(jcxz)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.cx==0)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jcxz_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -83,10 +95,12 @@ make_helper_jcc(jecxz)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.ecx==0)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jecxz_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -95,11 +109,13 @@ make_helper_jcc(jecxz)
 make_helper_jcc(je)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
-	if(cpu.ZF==1)
+	if(cpu.ZF)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("je_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -107,10 +123,12 @@ make_helper_jcc(jg)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.ZF==0&&cpu.SF==cpu.OF)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jg_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -118,10 +136,12 @@ make_helper_jcc(jge)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.SF==cpu.OF)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jge_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -130,10 +150,12 @@ make_helper_jcc(jl)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.SF!=cpu.OF)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jl_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -141,10 +163,12 @@ make_helper_jcc(jle)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.ZF==1||cpu.SF!=cpu.OF)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jle_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -152,10 +176,12 @@ make_helper_jcc(jna)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.CF==1||cpu.ZF==1)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jna_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -163,10 +189,12 @@ make_helper_jcc(jnae)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.CF==1)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jnae_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -174,10 +202,12 @@ make_helper_jcc(jnb)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.CF==0)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jnb_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -185,10 +215,12 @@ make_helper_jcc(jnbe)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.CF==0&&cpu.ZF==0)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jnbe_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -196,10 +228,12 @@ make_helper_jcc(jnc)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.CF==0)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jnc_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -207,10 +241,12 @@ make_helper_jcc(jne)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.ZF==0)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jne_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -219,10 +255,12 @@ make_helper_jcc(jng)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.ZF==1||cpu.SF!=cpu.OF)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jng_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -231,10 +269,12 @@ make_helper_jcc(jnge)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.SF!=cpu.OF)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jnge_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -242,10 +282,12 @@ make_helper_jcc(jnl)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.SF==cpu.OF)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jnl_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -255,10 +297,12 @@ make_helper_jcc(jnle)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.ZF==0&&cpu.SF==cpu.OF)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jnle_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -267,10 +311,12 @@ make_helper_jcc(jno)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.OF==0)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jno_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -279,10 +325,12 @@ make_helper_jcc(jnp)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.PF==0)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jnp_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -291,10 +339,12 @@ make_helper_jcc(jns)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.SF==0)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jns_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -303,10 +353,12 @@ make_helper_jcc(jnz)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.ZF==0)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jnz_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -316,10 +368,12 @@ make_helper_jcc(jo)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.OF==1)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jo_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -327,10 +381,12 @@ make_helper_jcc(jp)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.PF==1)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jp_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -339,10 +395,12 @@ make_helper_jcc(jpe)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.PF==1)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jpe_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -350,10 +408,12 @@ make_helper_jcc(jpo)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.PF==0)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jpo_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -361,10 +421,12 @@ make_helper_jcc(js)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.SF==1)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("js_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
@@ -372,10 +434,12 @@ make_helper_jcc(jz)
 {
 	concat(decode_rel_,SUFFIX)(eip+1);
 	if(cpu.ZF==1)
+	{
 	cpu.eip+=op_src->val;
 #if DATA_BYTE==2
 	cpu.eip=cpu.eip&0xFFFF;
 #endif
+	}
 	print_asm("jz_rel_"str(SUFFIX)" %s",op_src->str);
 	return 2;
 }
