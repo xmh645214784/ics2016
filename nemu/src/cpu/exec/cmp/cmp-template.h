d4a2db4e81d87377a26ba0e76ce261ed089de953
tracer-ics2016 <tracer@njuics.org>
2016-10-02 17:40:01 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:41:41 UTC 2016 i686 i686 i686 GNU/Linux  17:40:01 up  1:25,  2 users,  load average: 0.13, 0.16, 0.17 fe775ebe779ee0e63b4c08a6687ec89bd2901b0

--- nemu/src/cpu/exec/cmp/cmp-template.h
+++ nemu/src/cpu/exec/cmp/cmp-template.h
@@ -5,17 +5,17 @@
 //通用执行函数
 static void do_execute() {
 	//BUG?
-	DATA_TYPE_S src=-(DATA_TYPE_S)op_src->val;
-	DATA_TYPE_S des=(DATA_TYPE_S)op_dest->val;
+	DATA_TYPE src=(DATA_TYPE_S)op_src->val;
+	DATA_TYPE des=(DATA_TYPE_S)op_dest->val;
 	printf("%d\n", des);
-	Log("des-src=%x-%x",des,-src);
+	Log("des-src=%x-%x",des,src);
 	Log("DATA_BYTE=%d",DATA_BYTE);
-	CPU_AFFECT_CF(src,des,0)
+	CPU_AFFECT_CF(-src,des,0)
 
- 	CPU_AFFECT_ZF(src,des)
- 	CPU_AFFECT_PF(src,des)
- 	CPU_AFFECT_SF(src,des)
-  	CPU_AFFECT_OF(src,des,0)
+ 	CPU_AFFECT_ZF(-src,des)
+ 	CPU_AFFECT_PF(-src,des)
+ 	CPU_AFFECT_SF(-src,des)
+  	CPU_AFFECT_OF(-src,des,0)
 
 	print_asm_template2();
 }
