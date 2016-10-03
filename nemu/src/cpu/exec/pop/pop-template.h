e27f7d4bdf1e92ddeca6917b72ef0121fca39c6e
tracer-ics2016 <tracer@njuics.org>
2016-10-03 23:24:19 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:41:41 UTC 2016 i686 i686 i686 GNU/Linux  23:24:19 up  1:59,  2 users,  load average: 0.50, 0.31, 0.24 e0d4abf9ec5f869ddecd31ebb4dc012b7144e689

--- nemu/src/cpu/exec/pop/pop-template.h
+++ nemu/src/cpu/exec/pop/pop-template.h
@@ -7,8 +7,11 @@ extern CPU_state cpu;
 //通用执行函数
 static void do_execute() {
 	OPERAND_W(op_src,swaddr_read(cpu.esp,DATA_BYTE));
-
+#if DATA_BYTE==2
 	cpu.esp+=DATA_BYTE;
+#else
+	cpu.esp+=4;
+#endif
 	print_asm_template1();
 }
 
