71920bef63a4cc39480c34d40c3a42f2270c2214
tracer-ics2016 <tracer@njuics.org>
2016-10-03 23:43:11 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:41:41 UTC 2016 i686 i686 i686 GNU/Linux  23:43:11 up  2:18,  2 users,  load average: 0.11, 0.18, 0.23 c4189f366505f71ad5887190acfe2961afebec

--- nemu/src/cpu/exec/call/call.c
+++ nemu/src/cpu/exec/call/call.c
@@ -1,15 +1,44 @@
 #include "cpu/exec/helper.h"
 
+make_helper(call_rel_w)
+{
+	decode_i_w(eip+1);
+	cpu.esp-=2;
+	swaddr_write(cpu.esp,4,cpu.ip+3);
+	cpu.eip=(cpu.eip+(int16_t)op_src->val)&0xFFFF;
+	print_asm("call "str(op_src->str));
+	return 3;
+}
 
+make_helper(call_rel_l)
+{
+	decode_i_w(eip+1);
+	cpu.esp-=4;
+	swaddr_write(cpu.esp,4,cpu.eip+5);
+	cpu.eip+=(int)op_src->val;
+	print_asm("call "str(op_src->str));
+	return 5;
+}
 
-#define DATA_BYTE 2
-#include "call-template.h"
-#undef DATA_BYTE
+make_helper(call_rm_w)
+{
+	decode_rm2r_w(eip+1);
+	cpu.esp-=2;
+	swaddr_write(cpu.esp,4,cpu.ip+3);
+	cpu.eip+=(int16_t)op_src->val;
+	print_asm("call "str(op_src->str));
+	return 2;
+}
 
-#define DATA_BYTE 4
-#include "call-template.h"
-#undef DATA_BYTEs
+make_helper(call_rm_l)
+{
+	decode_rm2r_l(eip+1);
+	cpu.esp-=4;
+	swaddr_write(cpu.esp,4,cpu.eip+5);
+	cpu.eip+=(int)op_src->val;
+	print_asm("call "str(op_src->str));
+	return 2;
+}
 
-/* for instruction encoding overloading */
 make_helper_v(call_rel)
 make_helper_v(call_rm)
