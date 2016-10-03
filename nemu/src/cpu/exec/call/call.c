f7f91386278d007ba8d3cdeac0d885c3450c426c
tracer-ics2016 <tracer@njuics.org>
2016-10-03 23:53:03 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:41:41 UTC 2016 i686 i686 i686 GNU/Linux  23:53:03 up  2:27,  2 users,  load average: 0.19, 0.84, 0.81 68538415f423477d74ef0aef4a61f560d968e566

--- nemu/src/cpu/exec/call/call.c
+++ nemu/src/cpu/exec/call/call.c
@@ -4,8 +4,8 @@ make_helper(call_rel_w)
 {
 	decode_i_w(eip+1);
 	cpu.esp-=2;
-	swaddr_write(cpu.esp,4,cpu.ip+3);
-	cpu.eip=(cpu.eip+(int16_t)op_src->val)&0xFFFF;
+	swaddr_write(cpu.esp,2,cpu.ip+3);
+	cpu.eip=(cpu.eip+(int16_t)op_src->val)&0x0000FFFF;
 	print_asm("call "str(op_src->str));
 	return 3;
 }
@@ -24,20 +24,20 @@ make_helper(call_rm_w)
 {
 	decode_rm2r_w(eip+1);
 	cpu.esp-=2;
-	swaddr_write(cpu.esp,4,cpu.ip+3);
-	cpu.eip+=(int16_t)op_src->val;
+	swaddr_write(cpu.esp,2,cpu.ip+2);
+	cpu.eip=op_src->val&0x0000FFFFF;
 	print_asm("call "str(op_src->str));
-	return 2;
+	return 0;
 }
 
 make_helper(call_rm_l)
 {
 	decode_rm2r_l(eip+1);
 	cpu.esp-=4;
-	swaddr_write(cpu.esp,4,cpu.eip+5);
-	cpu.eip+=(int)op_src->val;
+	swaddr_write(cpu.esp,4,cpu.eip+2);
+	cpu.eip=op_src->val;
 	print_asm("call "str(op_src->str));
-	return 2;
+	return 0;
 }
 
 make_helper_v(call_rel)
