3c5288f8b356ba0d329ebb575d1d0c61254564e5
tracer-ics2016 <tracer@njuics.org>
2016-10-10 10:37:30 +0800

    >  run 151220131 xmh Linux ubuntu 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:41:41 UTC 2016 i686 i686 i686 GNU/Linux  10:37:30 up 29 min,  2 users,  load average: 0.10, 0.09, 0.18 a3abbb79a3b57f0e90571f8079b03dc5143748a4

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -44,7 +44,21 @@ FLOAT f2F(float a) {
 
 	void* point=&a;
 	int float_int=*(int *)point;
-	return 0;
+	unsigned exp=(float_int>>23)&0xFF-127;
+	int sign=(float_int>>31)&1;
+	unsigned e=float_int&0x7FFFFF;
+	e=e|0x800000;// 1+23  duo<<le 7   1---24   1 should be at 17
+	exp=exp-7;
+	if(e>>7)//-exp
+	{
+		e=e>>((-exp)&0xFF);
+	}
+	else//+exp
+	{
+		e=e<<exp;
+	}
+
+	return e;
 
 }
 
