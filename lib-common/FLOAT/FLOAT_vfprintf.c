d3a7ee189f7b424c4c9123bcf1f35515a16e5d09
tracer-ics2016 <tracer@njuics.org>
2016-10-30 20:08:42 +0800

    >  run 151220131 xmh Linux ubuntu 4.4.0-45-generic #66-Ubuntu SMP Wed Oct 19 14:12:05 UTC 2016 i686 i686 i686 GNU/Linux  20:08:42 up  3:13,  2 users,  load average: 0.23, 0.16, 0.10 9c8068476c441b19c84c8f18e11882701b4a717f

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -63,7 +63,7 @@ static void modify_vfprintf() {
 	 */
 	 //printf("%08x %08x\n", &_fpmaxtostr,&_vfprintf_internal);
 	
-mprotect((void *)(((unsigned int)&_vfprintf_internal + 0x306 - 100) & 0xfffff000), 4096*2, PROT_READ | PROT_WRITE | PROT_EXEC);
+//mprotect((void *)(((unsigned int)&_vfprintf_internal + 0x306 - 100) & 0xfffff000), 4096*2, PROT_READ | PROT_WRITE | PROT_EXEC);
 	 int a=*(int *)(&_vfprintf_internal+775);
 	 //printf("before hijack:%08x\n",*(int *)(&_vfprintf_internal+775));
 	 //printf("old jup=%d  %08x\n",4+(int)(&_vfprintf_internal+775)+a,4+(int)(&_vfprintf_internal+775)+a);
