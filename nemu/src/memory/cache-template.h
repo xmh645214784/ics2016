e510def24b87294b092968c23c99256f9879c662
tracer-ics2016 <tracer@njuics.org>
2016-11-02 15:54:18 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-45-generic #66-Ubuntu SMP Wed Oct 19 14:12:05 UTC 2016 i686 i686 i686 GNU/Linux  15:54:18 up 42 min,  2 users,  load average: 0.34, 0.23, 0.19 7700316968efdc23f7f26932a72faed39866acdb

--- nemu/src/memory/cache-template.h
+++ nemu/src/memory/cache-template.h
@@ -1,8 +1,13 @@
 #include "macro.h"
 #include "common.h"
-#define WAY (1<<LOG2_WAY)
 #define BLOCK_SIZE (1<<LOG2_BLOCK_SIZE)
-#define ADDRNOTE (32-LOG2_WAY-LOG2_BLOCK_SIZE)
+#define CACHE_SIZE (1<<LOG2_CACHE_SIZE)
+#define WAY (1<<LOG2_WAY)
+
+#define LOG2_NR_GROUP (LOG2_CACHE_SIZE-LOG2_BLOCK_SIZE-LOG2_WAY)
+
+
+#define ADDRNOTE (32-LOG2_NR_GROUP-LOG2_BLOCK_SIZE)
 #define NR_OF_CACHELINE (CACHE_SIZE/BLOCK_SIZE)
 #define NR_GROUP (NR_OF_CACHELINE/(WAY))
 
@@ -29,23 +34,49 @@ typedef struct
 #endif
 
 
+#define CACHE_OBJECT concat3(l,LEVEL,cache)
 
 
+/*For Initialize*/
+/*prototype: init_L1Cache*/
 void concat(init_,CACHE_NAME)()
 {
+	Assert((1<<LOG2_NR_GROUP)==NR_GROUP,"group index caculate failed");
+
 	/*CACHE*/
 	int i=0;
 	for(;i<NR_OF_CACHELINE;i++)
-		concat3(l,LEVEL,cache).cacheline[i].valid=0;
-	//prototype l1cache l2cache
+		CACHE_OBJECT.cacheline[i].valid=0;
+	/*prototype l1cache l2cache*/
 }
 
 
 
+int concat(read_,CACHE_NAME)(void *dest,hwaddr_t addr,size_t len)
+{
+	int i;
+	uint32_t groupindex=((addr>>LOG2_BLOCK_SIZE)%NR_GROUP)*NR_GROUP;
+	/*each group first element's index*/
+
+	Assert(groupindex>=0&&groupindex<NR_GROUP-WAY,"group index caculate failed");
+	for(i=0;i<WAY;i++)
+	{
+		if(CACHE_OBJECT.cacheline[groupindex+i].valid==1&&CACHE_OBJECT.cacheline[groupindex+i].addrnote==(addr>>(LOG2_NR_GROUP+LOG2_BLOCK_SIZE)))
+		{
+			memcpy(dest,&CACHE_OBJECT.cacheline[groupindex+i].data[addr%BLOCK_SIZE],len);
+			return 1;
+		}
+	}
+
+	return 0;
+}
 
 
+
+#undef CACHE_OBJECT
 
 #undef LEVEL
+
 
 #undef WAY
 #undef BLOCK_SIZE
@@ -53,8 +84,13 @@ void concat(init_,CACHE_NAME)()
 #undef NR_OF_CACHELINE
 #undef NR_GROUP
 
+
+#undef LOG2_CACHE_SIZE
 #undef LOG2_BLOCK_SIZE
 #undef LOG2_WAY
+#undef LOG2_NR_GROUP
+
+
 #undef CACHE_SIZE
 #undef CACHE_NAME
 
--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -1,7 +1,7 @@
 /*level 1 cache*/
 #define LOG2_BLOCK_SIZE (6)
 #define LOG2_WAY (3)
-#define CACHE_SIZE (64*1024)
+#define LOG2_CACHE_SIZE (16)
 #define CACHE_NAME L1Cache
 #define NOT_WRITE_ALLOCATE
 #define LEVEL 1
@@ -9,8 +9,8 @@
 #include "cache-template.h"
 
 #define LOG2_BLOCK_SIZE (6)
-#define LOG2_WAY (3)
-#define CACHE_SIZE (64*1024)
+#define LOG2_WAY (4)
+#define LOG2_CACHE_SIZE (22)
 #define CACHE_NAME L2Cache
 #define WRITE_ALLOCATE
 #define LEVEL 2
