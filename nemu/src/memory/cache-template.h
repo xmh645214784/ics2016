8be4997586621392b1e665ffd1dabedb873d32f8
tracer-ics2016 <tracer@njuics.org>
2016-11-03 23:28:10 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-45-generic #66-Ubuntu SMP Wed Oct 19 14:12:05 UTC 2016 i686 i686 i686 GNU/Linux  23:28:10 up 44 min,  2 users,  load average: 0.50, 0.25, 0.27 d9eeb3971a2561cf10602f35c6ae84cf6550aafd

--- nemu/src/memory/cache-template.h
+++ nemu/src/memory/cache-template.h
@@ -149,7 +149,7 @@ Assert(addr==((get_addr_note<<(LOG2_BLOCK_SIZE+LOG2_NR_GROUP))|(get_group_index<
 /*prototype :write_L1Cache write_L2Cache*/
 void concat(write_,CACHE_NAME)(uint32_t src,hwaddr_t addr,size_t len)
 {
-	Log("write:len=%d",len);
+	Log("write:len=%d addr=%08x",len,addr);
 	int i;
 	uint32_t group_index_in_array=get_group_index_in_array;
 
@@ -226,7 +226,7 @@ void concat(write_,CACHE_NAME)(uint32_t src,hwaddr_t addr,size_t len)
 /*prototype :read_L1Cache read_L2Cache*/
 uint32_t concat(read_,CACHE_NAME)(hwaddr_t addr,size_t len)
 {
-	Log("read:len=%d",len);
+	Log("read:len=%d addr=%08x",len,addr);
 	Log("group_index_in_array=%d  get_group_index_in_array=%d",get_group_index,get_group_index_in_array);
 	Log("%d  %d",NR_GROUP,WAY);
 	uint32_t group_index_in_array=get_group_index_in_array;
