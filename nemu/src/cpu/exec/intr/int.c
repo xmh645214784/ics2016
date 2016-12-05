2757b633c2bab474e469dac4ad423ae60f55d768
tracer-ics2016 <tracer@njuics.org>
2016-12-05 19:18:04 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-51-generic #72-Ubuntu SMP Thu Nov 24 18:29:17 UTC 2016 i686 i686 i686 GNU/Linux  19:18:04 up 50 min,  2 users,  load average: 1.66, 0.93, 0.75 9e17461b2858f52fe97553999bf772c6f70bf92

--- nemu/src/cpu/exec/intr/int.c
+++ nemu/src/cpu/exec/intr/int.c
@@ -6,11 +6,40 @@ make_helper(int_i_b)
 
 	void raise_intr(uint8_t NO);
 	
-
+/**
+ * The INT instruction generates via software a call to an interrupt
+handler. The immediate operand, from 0 to 255, gives the index number
+into the Interrupt Descriptor Table (IDT) of the interrupt routine to be
+called.
+ */
 #ifdef DEBUG
-	assert((int)op_src->val>0);
+	assert((int)op_src->val>=0&&(int)op_src->val<=255);
 #endif	
+	
+	cpu.eip+=2;
+//This is important
+
 	raise_intr(op_src->val);
 	print_asm("int 0x%02x",op_src->val);
 	return 1+len;
+}
+/*
+make_helper(int3)
+{
+	void raise_intr(uint8_t NO);
+	raise_intr(3);
+
+	print_asm("int 3");
+	cpu.eip+=1;
+	return 1;
+}*/
+
+make_helper(int4)
+{
+	void raise_intr(uint8_t NO);
+	raise_intr(4);
+
+	print_asm("int 4");
+	cpu.eip+=1;
+	return 1;
 }
---
+++ nemu/src/cpu/exec/intr/int.h
@@ -0,0 +1,3 @@
+make_helper(int_i_b);
+make_helper(int3);
+make_helper(int4);
