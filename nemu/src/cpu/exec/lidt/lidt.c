b2e85f9033db66ed72fe33b07051bcf2cf98eac3
tracer-ics2016 <tracer@njuics.org>
2016-12-05 22:57:22 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-51-generic #72-Ubuntu SMP Thu Nov 24 18:29:17 UTC 2016 i686 i686 i686 GNU/Linux  22:57:22 up 11 min,  2 users,  load average: 1.33, 1.79, 1.36 5ffa17be41bdcd55da5a638a8b47bbe794b7e087

--- nemu/src/cpu/exec/lidt/lidt.c
+++ nemu/src/cpu/exec/lidt/lidt.c
@@ -3,8 +3,7 @@
 
 static inline void do_execute() { // move gpr to cr
 #ifdef DEBUG
-	extern Operands ops_decoded;
-	Assert(ops_decoded.is_operand_size_16==0,"lgdt 16bit");
+	Assert(ops_decoded.is_operand_size_16==0,"lidt 16bit");
 #endif
 	print_asm("lidt fword %s", op_src->str);
 	cpu.idtr = lnaddr_read(op_src->addr + 2, 4);
