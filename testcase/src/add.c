e29d15fb52770fa1b1d2e9696f5dfe0271f11fac
tracer-ics2016 <tracer@njuics.org>
2016-12-07 19:41:21 +0800

    >  run 151220131 xmh Linux ubuntu 4.4.0-51-generic #72-Ubuntu SMP Thu Nov 24 18:29:17 UTC 2016 i686 i686 i686 GNU/Linux  19:41:21 up  1:20,  2 users,  load average: 0.47, 0.50, 0.46 45cc26523eb765e3dcccd6977b123abdbe8fa95d

--- testcase/src/add.c
+++ testcase/src/add.c
@@ -11,6 +11,7 @@ char str[] = "abcdefg";
 #define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))
 
 int main() {
+	*NULL;
 	int i, j, ans_idx = 0;
 	int loop = 0;
 	for(i = 0; i < NR_DATA; i ++) {
