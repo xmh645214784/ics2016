6222af15fc58de163f8e8b11fc274b963318828e
tracer-ics2016 <tracer@njuics.org>
2016-12-09 15:32:10 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-51-generic #72-Ubuntu SMP Thu Nov 24 18:29:17 UTC 2016 i686 i686 i686 GNU/Linux  15:32:10 up 25 min,  2 users,  load average: 0.09, 0.29, 0.41 84868426147346fc26d0ae1cb78079593ecd5ea3

--- nemu/src/cpu/exec/intr/iret.c
+++ nemu/src/cpu/exec/intr/iret.c
@@ -1,8 +1,8 @@
 #include "cpu/exec/helper.h"
 make_helper(iretd)
 {
-	if(cpu.cr0.protect_enable==0)
-	{
+	// if(cpu.cr0.protect_enable==0)
+	// {
 		if(ops_decoded.is_operand_size_16)
 		{
 			// Log("need notice")
@@ -28,57 +28,56 @@ extern void load_segment(int index,Selector selector);
 			cpu.EFLAGS=swaddr_read(cpu.esp,4,SR_SS);
 			cpu.esp+=4;
 		}
-	}
-	else
-	{
-		//EFLAGS
-		cpu.EFLAGS=swaddr_read(cpu.esp+8,4,SR_SS);
+// 	}
+// 	else
+// 	{
+// 		//EFLAGS
+// 		cpu.EFLAGS=swaddr_read(cpu.esp+8,4,SR_SS);
 		
-		//EIP
-		cpu.eip=swaddr_read(cpu.esp,4,SR_SS);
-		cpu.esp+=4;
+// 		//EIP
+// 		cpu.eip=swaddr_read(cpu.esp,4,SR_SS);
+// 		cpu.esp+=4;
 
-		//CS
-		Selector temp;
-		temp.val=swaddr_read(cpu.esp,4,SR_SS);
-extern void load_segment(int index,Selector selector);
-		load_segment(SR_CS,temp);
-		cpu.esp+=4;		
+// 		//CS
+// 		Selector temp;
+// 		temp.val=swaddr_read(cpu.esp,4,SR_SS);
+// extern void load_segment(int index,Selector selector);
+// 		load_segment(SR_CS,temp);
+// 		cpu.esp+=4;		
 
-//throwaway ← Pop(); (* pop away EFLAGS already read *)
-		cpu.esp+=4;
-		assert(0);
-		//ES
-		temp.val=swaddr_read(cpu.esp,4,SR_SS);
-		load_segment(SR_ES,temp);
-		cpu.esp+=4;		
+// //throwaway ← Pop(); (* pop away EFLAGS already read *)
+// 		cpu.esp+=4;
+// 		//ES
+// 		temp.val=swaddr_read(cpu.esp,4,SR_SS);
+// 		load_segment(SR_ES,temp);
+// 		cpu.esp+=4;		
 
-		//DS
-		temp.val=swaddr_read(cpu.esp,4,SR_SS);
-		load_segment(SR_DS,temp);
-		cpu.esp+=4;	
+// 		//DS
+// 		temp.val=swaddr_read(cpu.esp,4,SR_SS);
+// 		load_segment(SR_DS,temp);
+// 		cpu.esp+=4;	
 
-		//FS 
-		cpu.esp+=4;
+// 		//FS 
+// 		cpu.esp+=4;
 
-		//GS
-		cpu.esp+=4;
+// 		//GS
+// 		cpu.esp+=4;
 
-		uint32_t tempesp=swaddr_read(cpu.esp,4,SR_SS);
-		cpu.esp+=4;
-		uint32_t tempss=swaddr_read(cpu.esp,4,SR_SS);
-		cpu.esp+=4;
-		cpu.esp=tempesp;
+// 		uint32_t tempesp=swaddr_read(cpu.esp,4,SR_SS);
+// 		cpu.esp+=4;
+// 		uint32_t tempss=swaddr_read(cpu.esp,4,SR_SS);
+// 		cpu.esp+=4;
+// 		cpu.esp=tempesp;
 
-/**
- * load SS:ESp
- */
+// /**
+//  * load SS:ESp
+//  */
 		
-		temp.val=tempss;
-		load_segment(SR_SS,temp);
+// 		temp.val=tempss;
+// 		load_segment(SR_SS,temp);
 		
 
-	}
+// 	}
 	print_asm("iretd");
 	return 0;
 }
