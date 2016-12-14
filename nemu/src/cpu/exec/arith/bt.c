4d422d213dfa8979431cb43a8a70d6483c1c2801
tracer-ics2016 <tracer@njuics.org>
2016-12-14 12:38:51 +0800

    >  compile NEMU 151220131 xmh Linux ubuntu 4.4.0-53-generic #74-Ubuntu SMP Fri Dec 2 15:58:04 UTC 2016 i686 i686 i686 GNU/Linux  12:38:51 up 16 min,  2 users,  load average: 1.17, 0.79, 0.62 19241cebd05f7678ab7cb17377dbf77dc7cc3c2

--- nemu/src/cpu/exec/arith/bt.c
+++ nemu/src/cpu/exec/arith/bt.c
@@ -1,18 +1,18 @@
 #include "cpu/exec/helper.h"
 make_helper(bt_r2rm_w)
 {
-	decode_r2rm_w(eip+1);
+	int len=decode_r2rm_w(eip+1);
 	cpu.CF=(op_src->val>>op_dest->val)&1;
 	print_asm("bt %s, %s",op_src->str,op_dest->str);
-	return 1;
+	return 1+len;
 }
 
 make_helper(bt_r2rm_l)
 {
-	decode_r2rm_l(eip+1);
+	int len=decode_r2rm_l(eip+1);
 	cpu.CF=(op_src->val>>op_dest->val)&1;
 	print_asm("bt %s, %s",op_src->str,op_dest->str);
-	return 1;
+	return 1+len;
 }
 
 make_helper_v(bt_r2rm)
